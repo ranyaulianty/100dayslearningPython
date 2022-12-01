4dda0cdb261416ed4819980ebf817e2e72dbfb5f
ranyaulianty <114273008+ranyaulianty@users.noreply.github.com>
2022-12-01 20:05:10 +0700

    Create day1project.py

---
+++ day1project.py
@@ -0,0 +1,8 @@
+# A greeting to the user
+print ("Henlo and welcome to a band name generator")
+# Ask the user to input the city where they live
+city = input ("On what city are you living in?\n")
+# Ask the user to input their favorite character from The Simpsons
+character = input ("Who's your favorite character from The Simpsons?\n")
+#The band name is the combination of the answers given
+print ("So, your band name would be " + city + " " + character)
